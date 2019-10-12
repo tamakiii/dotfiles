f81ef41ebdd99a0aac3dfeed8e1d4bd17c42b5e5
tamakiii <tamakiii@users.noreply.github.com>
2019-10-12 13:55:02 +0900

    fix-20191012

---
+++ etc/init/init.sh
@@ -0,0 +1,4 @@
+#!env bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
