bc915b4d4542b88a4f4cf2c0189161f89432464f
tamakiii <tamakiii@users.noreply.github.com>
2019-10-12 19:27:55 +0900

    add fish_prompt.fish

--- config.mk
+++ config.mk
@@ -5,9 +5,11 @@ all: \
 	~/.config/git \
 	~/.config/git/config \
 	~/.config/fish \
+	~/.config/fish/functions \
 	~/.config/fish/config.fish \
 	~/.config/fish/fishfile \
-	~/.config/fish/fish_variables
+	~/.config/fish/fish_variables \
+	~/.config/fish/functions/fish_prompt.fish
 
 ~/.config/git/config:
 	cp $(ROOT_DIR)/$(subst $(HOME)/,,$@) $@
@@ -21,6 +23,12 @@ all: \
 ~/.config/fish/fish_variables:
 	ln -s $(ROOT_DIR)/$(subst $(HOME)/,,$@) $@
 
+~/.config/fish/functions/fish_prompt.fish:
+	ln -s $(ROOT_DIR)/$(subst $(HOME)/,,$@) $@
+
+~/.config/fish/functions:
+	mkdir -p $@
+
 ~/.config/fish:
 	mkdir -p $@
 
@@ -32,8 +40,9 @@ all: \
 
 clean:
 	cp -r ~/.config $(shell mktemp -d)
-	rm -rf ~/.config/git/config
+	# rm -rf ~/.config/git/config
 	rm -rf ~/.config/fish/config.fish
 	rm -rf ~/.config/fish/fishfile
 	rm -rf ~/.config/fish/fish_variables
+	rm -rf ~/.config/fish/functions/fish_prompt.fish
 
