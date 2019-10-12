d3d86d26fe3c19447b7f9df65c4d8e5e63995fba
tamakiii <tamakiii@users.noreply.github.com>
2019-10-12 16:49:27 +0900

    mv to brew.mk

---
+++ make/mac/brew.mk
@@ -0,0 +1,27 @@
+.PHONY: Brewfile
+
+DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+
+all: install
+
+install:
+	@type brew > /dev/null
+	@brew update
+	@brew bundle --file=$(DIR)/Brewfile
+
+import:
+	@brew bundle dump --force --file=$(DIR)/Brewfile
+
+check:
+	@type brew > /dev/null
+	@brew bundle check --file=$(DIR)/Brewfile
+	@brew bundle cleanup --file=$(DIR)/Brewfile
+
+reinstall:
+	@type brew > /dev/null
+	@grep '^cask' Brewfile
+	@grep '^cask' Brewfile | awk '{print $2}' | xargs -I@ brew cask reinstall @
+
+clean:
+	@type brew > /dev/null
+	@brew bundle cleanup --force --file=$(DIR)/Brewfile
