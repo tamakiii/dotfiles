eaca69768d9ba89681be4064044c521aab3b9bda
tamakiii <tamakiii@users.noreply.github.com>
2019-11-24 11:32:31 +0900

    SKIP_BREW=1

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -5,11 +5,12 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)"
 make -C $DIR/etc/init
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  make -f $DIR/brew.mk
+  if [[ "$SKIP_BREW" != "1" ]]; then
+    make -f $DIR/brew.mk
+  fi
 fi
 
 make -f $DIR/config.mk
-
 
 fish -c "fisher"
 vim -E -s -u ~/.vimrc +PlugInstall +qall
