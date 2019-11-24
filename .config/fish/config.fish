9f5a604679873a2fe42df9150f0090df4ffc31fb
tamakiii <tamakiii@users.noreply.github.com>
2019-11-24 20:22:07 +0900

    setting LC

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
+set -xg LANG ""
+set -xg LC_COLLATE "ja_JP.UTF-8"
+set -xg LC_CTYPE "ja_JP.UTF-8"
+set -xg LC_MESSAGES "ja_JP.UTF-8"
+set -xg LC_MONETARY "ja_JP.UTF-8"
+set -xg LC_NUMERIC "ja_JP.UTF-8"
+set -xg LC_TIME "ja_JP.UTF-8"
+set -xg LC_ALL "ja_JP.UTF-8"
+
 set -U FZF_LEGACY_KEYBINDINGS 0
 set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
 
