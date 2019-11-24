40644c715c015c703e43bb9630615297bfb65bf4
tamakiii <tamakiii@users.noreply.github.com>
2019-11-24 10:48:01 +0900

    fixes-20191124

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -2,11 +2,13 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)"
 
+make -C $DIR/etc/init
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   make -f $DIR/brew.mk
 fi
 
 make -f $DIR/config.mk
-make -C $DIR/etc/init
+
 
 fish -c "fisher"
