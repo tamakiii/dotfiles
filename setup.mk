bda63dc57e34cc75649266dc2677f24208a3cdef
tamakiii <tamakiii@users.noreply.github.com>
2020-03-31 23:58:18 +0900

    fixes

--- setup.mk
+++ setup.mk
@@ -1,16 +1,21 @@
-.PHONY: help install chsh clean
+.PHONY: help deps install chsh clean
 
 help: ## Self-documented Makefile
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(lastword $(MAKEFILE_LIST)) \
 		| awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-chsh: /etc/shells
+deps:
+	@type brew > /dev/null
+
+install: ## Show dot files in this repo
+	$(brew --prefix)/opt/fzf/install
+
+chsh: /etc/shells ## Change shell
 	grep $(TARGET) $^ || (echo "Shell $(TARGET) not found in /etc/shells"; exit 1)
 	chsh --shell $(TARGET) tecmint
 	# Open `Preferences > Users & Groups > Right click icon -> Advanced Options` Set `Login shell``
-	dscl . change /users/$(whoami) UserShell /bin/bash $(TARGET)
-
-install: ## Show dot files in this repo
+	# and restart OS
 
 clean:
+	$(brew --prefix)/opt/fzf/uninstall
 
