0325c67694d7f6dd6d6fc227f3c11a5946f82b56
tamakiii <tamakiii@users.noreply.github.com>
2020-04-01 01:06:40 +0900

    using etc/init/Makefile

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -4,13 +4,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   if [[ "$SKIP_BREW" != "1" ]]; then
-    make -f $DIR/brew.mk
+    make -C $DIR -f brew.mk
   fi
 fi
 
-vim -E -s -u ~/.vimrc +PlugInstall +qall
-$DIR/.tmux/plugins/tpm/tpm
-
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/swapfile
-mkdir -p ~/.vim/undo
+make -C $DIR install
