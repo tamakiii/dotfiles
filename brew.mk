bf4652d81fb64c79bccfbf2a11981d3b4d6280a7
tamakiii <tamakiii@users.noreply.github.com>
2020-07-05 17:36:25 +0900

    fix brew.mk

--- brew.mk
+++ brew.mk
@@ -1,7 +1,6 @@
-.PHONY: help dependencies install update upgrade uninstsall diff check cleanup reinstall unshallow clean
+.PHONY: help dependencies install build diff unshallow clean
 
 SHELL := bash
-
 DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 
 help:
@@ -18,39 +17,11 @@ install: \
 build:
 	brew bundle install
 
-update:
-	brew update
-
-upgrade:
-	brew upgrade
-
 Brewfile:
 	brew bundle dump --file=/dev/stdout | sort > $@
 
-uninstall:
-	diff -u <(brew bundle list --taps) <(cat Brewfile | grep '^tap' | sed 's/tap[ ]*"\(.*\)"/\1/') | xargs brew untap
-	diff -u <(brew bundle list --brews) <(cat Brewfile | grep '^brew' | sed 's/brew[ ]*"\(.*\)"/\1/') | xargs brew uninstall
-	diff -u <(brew bundle list --casks) <(cat Brewfile | grep '^cask' | sed 's/cask[ ]*"\(.*\)"/\1/') | xargs brew cask uninstall
-
 diff:
 	@diff -u <(cat Brewfile | sort) <(brew bundle dump --file=/dev/stdout | sort)
-
-diff-details:
-	diff -u <(brew bundle list --taps) <(cat Brewfile | grep '^tap' | sed 's/tap[ ]*"\(.*\)"/\1/') || true
-	diff -u <(brew bundle list --brews) <(cat Brewfile | grep '^brew' | sed 's/brew[ ]*"\(.*\)"/\1/') || true
-	diff -u <(brew bundle list --casks) <(cat Brewfile | grep '^cask' | sed 's/cask[ ]*"\(.*\)"/\1/') || true
-
-check:
-	brew bundle check
-
-cleanup:
-	brew bundle cleanup
-
-reinstall:
-	grep '^cask' Brewfile | awk '{print $2}' | xargs -I@ brew cask reinstall @
-
-update-reset:
-	brew update-reset
 
 unshallow:
 	git -C "$$(brew --repo homebrew/core)" fetch --unshallow
