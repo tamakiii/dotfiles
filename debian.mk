96598876ca40c99008744b7269a1fb187f282293
tamakiii <tamakiii@users.noreply.github.com>
2022-08-09 19:30:39 +0900

    add install-locales

--- debian.mk
+++ debian.mk
@@ -1,10 +1,10 @@
-.PHONY: help install install-packages update clean
+.PHONY: help install update clean
+.PHONY: install-packages install-locales install-shell
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
-install: install-packages
-	chsh -s $(shell which zsh)
+install: install-packages install-locales install-shell
 	$(MAKE) -f vim.mk install
 
 install-packages:
@@ -15,7 +15,22 @@ install-packages:
 	  curl \
 	  ca-certificates \
 	  zsh \
+	  locales \
+	  less \
 	  man
+
+install-locales: \
+	/usr/share/i18n \
+	/usr/lib/locale/en_US.UTF-8
+
+install-shell: | /etc/shells /etc/passwd
+	chsh -s $(shell which zsh)
+
+/usr/share/i18n:
+	locale-gen
+
+/usr/lib/locale/en_US.UTF-8: | /usr/share/i18n/locales/en_US
+	localedef -i $| -f UTF-8 $@
 
 update:
 	apt update
