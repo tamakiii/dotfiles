43ea85774047ecf99f5f17337808af6f7aca3ee4
tamakiii <tamakiii@users.noreply.github.com>
2023-03-18 14:10:56 +0900

    init dependency.mk

---
+++ dependency.mk
@@ -0,0 +1,23 @@
+.PHONY: help setup install
+.PHONY: teardown clean
+
+help:
+	@cat $(firstword $(MAKEFILE_LIST))
+
+setup: \
+	dependency/artempyanykh/marksman
+
+install:
+	make -C dependency/artempyanykh/marksman install
+
+dependency/artempyanykh/marksman: | dependency
+	git clone https://github.com/artempyanykh/marksman.git $@
+
+dependency:
+	mkdir -p $@
+
+teardown:
+	rm -rf dependency
+
+clean:
+	//
