f476ce98f4980900ade65c8630f9bf51d49fdbb7
tamakiii <tamakiii@users.noreply.github.com>
2023-03-20 23:57:10 +0900

    install .dotfiles/.venv

--- poetry.mk
+++ poetry.mk
@@ -1,23 +1,13 @@
-.PHONY: help setup install teardown uninstall
-
-DIR_VIRTUALENV := $(shell python3 -m poetry env info -p)
+.PHONY: help install uninstall
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
-setup:
-	python3 -m poetry config --local virtualenvs.create false
-	python3 -m poetry config --local virtualenvs.prompt null
-
 install: \
-	$(DIR_VIRTUALENV)
-
-teardown:
-	python3 -m poetry config --local virtualenvs.create --unset
-	python3 -m poetry config --local virtualenvs.prompt --unset
+	.venv
 
 uninstall:
-	rm -rf $(DIR_VIRTUALENV)
+	rm -rf .venv
 
 pyproject.toml:
 	python3 -m poetry init
@@ -25,5 +15,5 @@ pyproject.toml:
 poetry.lock:
 	python3 -m poetry self lock --no-update
 
-$(DIR_VIRTUALENV):
+.venv:
 	python3 -m poetry install --no-cache --no-root
