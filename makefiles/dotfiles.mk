cc71c794a4fc49bca845544f8ca3a800c99cb7b1
Daichi Tamaki <tamakiii@Weimaraner.local>
2024-04-12 16:08:21 +0900

    add .zsh and .zshrc

--- makefiles/dotfiles.mk
+++ makefiles/dotfiles.mk
@@ -4,10 +4,20 @@ help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 install: \
-	~/.config
+	~/.config \
+	~/.zsh \
+	~/.zshrc
 
 uninstall:
+	rm -vrf ~/.zshrc
+	rm -vrf ~/.zsh
 	rm -vrf ~/.config
 
 ~/.config: ../.config
+	ln -sfnv $(abspath $<) $@
+
+~/.zsh: ../.zsh
+	ln -sfnv $(abspath $<) $@
+
+~/.zshrc: ../.zshrc
 	ln -sfnv $(abspath $<) $@
