b7802a0fc6fc25fe4b51fa7188a717893b96ebbf
tamakiii <tamakiii@users.noreply.github.com>
2024-11-04 11:07:59 +0900

    add some more

--- make/windows.mk
+++ make/windows.mk
@@ -1,14 +1,25 @@
-.PHONY: help install check uninstall
+.PHONY: help install check check-dependency check-winget uninstall
+
+SHELL := bash --noprofile --norc -eo pipefail
+error-install := echo "[error] install '$$_'"; exit 1;
+check-dependency =  winget list -q $(1) > /dev/null || { $(call error-install) }
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 install:
+	winget install Helix.Helix
 	winget install 9NT1R1C2HH7J # https://apps.microsoft.com/detail/9nt1r1c2hh7j
 
 check:
 	test -L ~/AppData/Roaming/helix/config.toml
 	test -L ~/AppData/Roaming/helix/themes
+
+check-dependency:
+	@$(call check-dependency,Helix.Helix)
+
+check-winget:
+	@$(call cehck-dependency,9NT1R1C2HH7J) # ChatGPT
 
 uninstall:
 	echo "not implemented yet"
