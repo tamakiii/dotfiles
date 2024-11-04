0d5d25dda014a0c53e21da3b0943acdbbcfe9670
tamakiii <tamakiii@users.noreply.github.com>
2024-11-04 11:25:45 +0900

    add check

--- makefiles/ubuntu.mk
+++ makefiles/ubuntu.mk
@@ -1,6 +1,8 @@
-.PHONY: help install update upgrade uninstall
+.PHONY: help install check update upgrade uninstall
 
 SHELL := bash --noprofile --norc -eo pipefail
+exit = echo "[error] $(2) '$(3)'"; exit $(1);
+current-shell = $(shell grep "$$(whoami)" /etc/passwd | awk -F : '{ print $$7 }')
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -9,6 +11,9 @@ install: update
 	apt install --no-install-recommends \
 		fzf \
 		zsh
+
+check:
+	@test "$$(which zsh)" = "$(call current-shell)" || { $(call exit,1,default shell should be zsh,$$_) }
 
 update:
 	apt update
