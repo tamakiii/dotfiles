7d9899b9b3e98d2e1dd4de9d0369357dcbf0af3c
tamakiii <tamakiii@users.noreply.github.com>
2025-07-13 18:24:55 +0900

    update-arch-20250713

--- makefiles/include/secret.mk
+++ makefiles/include/secret.mk
@@ -9,4 +9,9 @@ OS_NAME := $(shell uname)
 # Include macOS-specific secrets if on macOS
 ifeq ($(OS_NAME),Darwin)
 include makefiles/include/secret.macos.mk
+else
+GITHUB_PERSONAL_ACCESS_TOKEN =
+DISCORD_CHANNEL_ID_CLAUDE =
+DISCORD_USER_ID =
+DISCORD_TOKEN =
 endif
