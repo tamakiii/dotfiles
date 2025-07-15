9e210a1d7b4fb48df9dc6648d910e716fdafa889
tamakiii <tamakiii@users.noreply.github.com>
2025-07-16 07:13:19 +0900

    add status and update

--- dotcommands.mk
+++ dotcommands.mk
@@ -1,4 +1,6 @@
-.PHONY: help install uninstall
+.PHONY: help install uninstall status update
+
+git = git -C .dotcommands $1
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -8,6 +10,12 @@ install: \
 
 uninstall:
 	rm -rf .dotcommands
+
+status:
+	git -C .dotcommands status
+
+update:
+	git -C .dotcommands pull origin "$$(git -C .dotcommands rev-parse --abbrev-ref @)"
 
 .dotcommands:
 	git clone https://github.com/tamakiii/dotcommands.git $@
