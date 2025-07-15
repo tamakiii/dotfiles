f20d65fd68bacac29f4b5ee1bf916ffd5f54c6fa
tamakiii <tamakiii@users.noreply.github.com>
2025-07-16 08:04:04 +0900

    init os/arch/makefiles/secret.mk

---
+++ os/arch/makefiles/secret.mk
@@ -0,0 +1,10 @@
+.PHONY: @help
+
+@help:
+	@echo "Arch Linux secret makefile - uses gh CLI for GitHub token"
+
+
+# Discord tokens - empty on Arch for now
+DISCORD_CHANNEL_ID_CLAUDE =
+DISCORD_USER_ID =
+DISCORD_TOKEN =
